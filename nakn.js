2cd0f7cf48ee05aeba3747d8e5145dd858918d44
qatuno7 <123946611+qatuno7@users.noreply.github.com>
2023-02-01 19:37:32 +0100

    Update nakn.js

--- nakn.js
+++ nakn.js
@@ -1,21 +1,29 @@
-<script>
-  const form = document.querySelector("form");
-  const definitionDiv = document.querySelector("#definition");
+const form = document.querySelector("form");
+const definitionDiv = document.querySelector("#definition");
+let entries;
 
-  form.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const word = form.elements.word.value;
-
-    fetch("dictionary.json")
-      .then(response => response.json())
-      .then(data => {
-        const definition = data[word];
-        definitionDiv.innerHTML = definition || "Word not found.";
-      })
-      .catch(error => {
-        console.error("Error fetching dictionary data:", error);
-        definitionDiv.innerHTML = "Error fetching dictionary data.";
-      });
+fetch("dictionary.txt")
+  .then(response => response.text())
+  .then(data => {
+    entries = data.split("\n");
+    entries = entries.reduce((dictionary, entry) => {
+      const parts = entry.split(":");
+      const word = parts[0];
+      const definition = parts[1];
+      dictionary[word] = definition;
+      return dictionary;
+    }, {});
+  })
+  .catch(error => {
+    console.error("Error fetching dictionary data:", error);
+    definitionDiv.innerHTML = "Error fetching dictionary data.";
   });
-</script>
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const word = form.elements.word.value;
+  const definition = entries[word];
+
+  definitionDiv.innerHTML = definition || "Word not found.";
+});
