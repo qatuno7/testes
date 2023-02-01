5a3a9f6c005bdaa6efdf723be2bc41fba783fbfc
qatuno7 <123946611+qatuno7@users.noreply.github.com>
2023-02-01 20:22:09 +0100

    Update nakn.js

--- nakn.js
+++ nakn.js
@@ -1,29 +1,39 @@
 const form = document.querySelector("form");
-const definitionDiv = document.querySelector("#definition");
-let entries;
+    const definitionDiv = document.querySelector("#definition");
+    let entries;
+    let wordToDefinition = {};
+    let definitionToWord = {};
 
-fetch("dictionary.txt")
-  .then(response => response.text())
-  .then(data => {
-    entries = data.split("\n");
-    entries = entries.reduce((dictionary, entry) => {
-      const parts = entry.split(":");
-      const word = parts[0];
-      const definition = parts[1];
-      dictionary[word] = definition;
-      return dictionary;
-    }, {});
-  })
-  .catch(error => {
-    console.error("Error fetching dictionary data:", error);
-    definitionDiv.innerHTML = "Error fetching dictionary data.";
-  });
+    fetch("dictionary.txt")
+      .then(response => response.text())
+      .then(data => {
+        entries = data.split("\n");
+        entries.forEach(entry => {
+          const parts = entry.split(":");
+          const word = parts[0];
+          const definition = parts[1];
+          wordToDefinition[word] = definition;
+          definitionToWord[definition] = word;
+        });
+      })
+      .catch(error => {
+        console.error("Error fetching dictionary data:", error);
+        definitionDiv.innerHTML = "Error fetching dictionary data.";
+      });
 
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
 
-  const word = form.elements.word.value;
-  const definition = entries[word];
+      const query = form.elements.query.value;
+      let result;
 
-  definitionDiv.innerHTML = definition || "Word not found.";
-});
+      if (wordToDefinition[query]) {
+        result = wordToDefinition[query];
+      } else if (definitionToWord[query]) {
+        result = definitionToWord[query];
+      } else {
+        result = "Word or definition not found.";
+      }
+
+      definitionDiv.innerHTML = result;
+    });
